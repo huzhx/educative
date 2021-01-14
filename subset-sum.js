69f4e112bb470b0ac3095b48a9d0d3e07806fe06
Zhaoxian Hu <huzhx@uci.edu>
2021-01-13 21:13:33 -0800

    add subset-sum.js and its test

---
+++ subset-sum.js
@@ -0,0 +1,21 @@
+const subsetSum = (nums, sum) => {
+  const memo = [];
+  return backtrack(nums, sum, 0);
+
+  function backtrack(nums, sum, curIndex) {
+    if (nums[curIndex] === sum) return true;
+
+    if (curIndex === nums.length || nums[curIndex] > sum) return false;
+
+    memo[curIndex] = memo[curIndex] || [];
+    if (typeof memo[curIndex][sum] !== 'undefined') {
+      return memo[curIndex][sum];
+    }
+
+    memo[curIndex][sum] = backtrack(nums, sum - nums[curIndex], curIndex + 1) || backtrack(nums, sum, curIndex + 1);
+
+    return memo[curIndex][sum];
+  }
+};
+
+module.exports = subsetSum;
---
+++ subset-sum.test.js
@@ -0,0 +1,23 @@
+const { expect } = require('@jest/globals');
+const subsetSum = require('./subset-sum');
+
+test('test1', () => {
+  const input = [1, 2, 3, 7];
+  const sum = 6;
+  const result = subsetSum(input, sum);
+  expect(result).toBeTruthy();
+});
+
+test('test2', () => {
+  const input = [1, 2, 7, 1, 5];
+  const sum = 10;
+  const result = subsetSum(input, sum);
+  expect(result).toBeTruthy();
+});
+
+test('test3', () => {
+  const input = [1, 3, 4, 8];
+  const sum = 6;
+  const result = subsetSum(input, sum);
+  expect(result).toBeFalsy();
+});
