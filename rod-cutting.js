5a9dd8bdace05484b911d1f02ae7923a02ce9960
Zhaoxian Hu <huzhx@uci.edu>
2021-02-07 10:32:14 -0800

    add rod-cutting

---
+++ rod-cutting.js
@@ -0,0 +1,31 @@
+const getMaxProfit = (lengths, prices, rodLength) => {
+  const memo = {};
+  return backtrack(lengths, prices, 0, rodLength);
+
+  function backtrack(lengths, prices, curIndex, remainingRodLength) {
+    if (
+      lengths.length === 0 ||
+      lengths.length !== prices.length ||
+      curIndex >= lengths.length ||
+      remainingRodLength < 0
+    ) {
+      return 0;
+    }
+
+    memo[curIndex] = memo[curIndex] || [];
+    if (typeof memo[curIndex][remainingRodLength] !== 'undefined') {
+      return memo[curIndex][remainingRodLength];
+    }
+
+    let profit1 = 0;
+    if (lengths[curIndex] <= remainingRodLength) {
+      profit1 = prices[curIndex] + backtrack(lengths, prices, curIndex, remainingRodLength - lengths[curIndex]);
+    }
+    let profit2 = backtrack(lengths, prices, curIndex + 1, remainingRodLength);
+
+    memo[curIndex][remainingRodLength] = Math.max(profit1, profit2);
+    return memo[curIndex][remainingRodLength];
+  }
+};
+
+export default getMaxProfit;
---
+++ rod-cutting.test.js
@@ -0,0 +1,10 @@
+import getMaxProfit from './rod-cutting';
+
+test('test1', () => {
+  const lengths = [1, 2, 3, 4, 5];
+  const prices = [2, 6, 7, 10, 13];
+  const rodLength = 5;
+  const expected = 14;
+  const result = getMaxProfit(lengths, prices, rodLength);
+  expect(result).toBe(expected);
+});
