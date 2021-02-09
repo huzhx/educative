f1ab41142122a6e35795a6be3a331e07de29e5bd
Zhaoxian Hu <huzhx@uci.edu>
2021-02-09 00:06:08 -0800

    add house-thief and its test

---
+++ house-thief.js
@@ -0,0 +1,21 @@
+const getMaxMoney = (input) => {
+  const memo = {};
+  return helper(input, 0);
+
+  function helper(input, curIndex) {
+    if (input.length === 0 || curIndex >= input.length) return 0;
+
+    if (typeof memo[curIndex] !== 'undefined') {
+      return memo[curIndex];
+    }
+
+    const money1 = helper(input, curIndex + 2) + input[curIndex];
+
+    const money2 = helper(input, curIndex + 1);
+
+    memo[curIndex] = Math.max(money1, money2);
+    return memo[curIndex];
+  }
+};
+
+export default getMaxMoney;
---
+++ house-thief.test.js
@@ -0,0 +1,15 @@
+import getMaxMoney from './house-thief';
+
+test('test1', () => {
+  const input = [2, 5, 1, 3, 6, 2, 4];
+  const expected = 15;
+  const result = getMaxMoney(input);
+  expect(result).toBe(expected);
+});
+
+test('test2', () => {
+  const input = [2, 10, 14, 8, 1];
+  const expected = 18;
+  const result = getMaxMoney(input);
+  expect(result).toBe(expected);
+});
