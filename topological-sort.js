5743b25e5ea7d0963b5c62e7e8fd21bef2a01cf6
Zhaoxian Hu <huzhx@uci.edu>
2021-02-13 15:39:23 -0800

    add topologicalSortByBFS and update the name of parent and child

--- topological-sort.js
+++ topological-sort.js
@@ -3,30 +3,74 @@ const topologicalSortByDFS = (vertices, edges) => {
   const adjMap = new Map();
   const topOrder = [];
 
-  for (let [child, parent] of edges) {
-    const parents = adjMap.get(child) || [];
-    parents.push(parent);
-    adjMap.set(child, parents);
+  for (let [parent, child] of edges) {
+    const children = adjMap.get(parent) || [];
+    children.push(child);
+    adjMap.set(parent, children);
   }
 
   for (let i = 0; i < visited.length; i++) {
     if (visited[i] === 0) {
-      dfs(i);
+      dfs(i, new Set([i]));
     }
   }
 
+  if (topOrder.length !== vertices) return [];
+
   return topOrder.reverse();
 
-  function dfs(node) {
+  function dfs(node, visiting) {
     visited[node] = 1;
-    const parents = adjMap.get(node) || [];
-    for (let parent of parents) {
-      if (visited[parent] === 0) {
-        dfs(parent);
+    const children = adjMap.get(node) || [];
+    for (let child of children) {
+      // * detect cycle
+      if (visiting.has(child)) {
+        return;
+      }
+      if (visited[child] === 0) {
+        visiting.add(child);
+        dfs(child, visiting);
+        visiting.delete(child);
       }
     }
     topOrder.push(node);
   }
 };
 
-export { topologicalSortByDFS };
+const topologicalSortByBFS = (vertices, edges) => {
+  const adjMap = new Map();
+  const indegrees = new Array(vertices).fill(0);
+  const topOrder = [];
+  const queue = [];
+
+  for (let [parent, child] of edges) {
+    const children = adjMap.get(parent) || [];
+    children.push(child);
+    adjMap.set(parent, children);
+    indegrees[child]++;
+  }
+
+  for (let i = 0; i < indegrees.length; i++) {
+    if (indegrees[i] === 0) {
+      queue.push(i);
+    }
+  }
+
+  while (queue.length) {
+    const cur = queue.shift();
+    topOrder.push(cur);
+    const children = adjMap.get(cur) || [];
+    children.forEach((child) => {
+      indegrees[child]--;
+      if (indegrees[child] === 0) {
+        queue.push(child);
+      }
+    });
+  }
+
+  if (topOrder.length !== vertices) return [];
+
+  return topOrder;
+};
+
+export { topologicalSortByDFS, topologicalSortByBFS };
--- topological-sort.test.js
+++ topological-sort.test.js
@@ -1,4 +1,18 @@
-import { topologicalSortByDFS } from './topological-sort';
+import { topologicalSortByDFS, topologicalSortByBFS } from './topological-sort';
+
+test('test0', () => {
+  const vertices = 4;
+  const edges = [
+    [3, 2],
+    [3, 0],
+    [2, 0],
+    [2, 1],
+    [1, 2],
+  ];
+  const result = topologicalSortByDFS(vertices, edges);
+  const expected = [];
+  expect(expected).toEqual(result);
+});
 
 test('test1', () => {
   const vertices = 4;
@@ -59,4 +73,115 @@ test('test3', () => {
     [6, 5, 4, 3, 2, 1, 0],
   ];
   expect(expected).toContainEqual(result);
+});
+
+test('test4', () => {
+  const vertices = 7;
+  const edges = [
+    [6, 4],
+    [6, 2],
+    [5, 3],
+    [5, 4],
+    [3, 0],
+    [3, 1],
+    [3, 2],
+    [4, 1],
+    [1, 4],
+  ];
+  const result = topologicalSortByDFS(vertices, edges);
+  const expected = [];
+  expect(expected).toEqual(result);
+});
+
+test('test5', () => {
+  const vertices = 4;
+  const edges = [
+    [3, 2],
+    [3, 0],
+    [2, 0],
+    [2, 1],
+    [1, 2],
+  ];
+  const result = topologicalSortByBFS(vertices, edges);
+  const expected = [];
+  expect(expected).toEqual(result);
+});
+
+test('test6', () => {
+  const vertices = 4;
+  const edges = [
+    [3, 2],
+    [3, 0],
+    [2, 0],
+    [2, 1],
+  ];
+  const result = topologicalSortByBFS(vertices, edges);
+  const expected = [
+    [3, 2, 0, 1],
+    [3, 2, 1, 0],
+  ];
+  expect(expected).toContainEqual(result);
+});
+
+test('test7', () => {
+  const vertices = 5;
+  const edges = [
+    [4, 2],
+    [4, 3],
+    [2, 0],
+    [2, 1],
+    [3, 1],
+  ];
+  const result = topologicalSortByBFS(vertices, edges);
+  const expected = [
+    [4, 2, 3, 0, 1],
+    [4, 3, 2, 0, 1],
+    [4, 3, 2, 1, 0],
+    [4, 2, 3, 1, 0],
+    [4, 2, 0, 3, 1],
+  ];
+  expect(expected).toContainEqual(result);
+});
+
+test('test8', () => {
+  const vertices = 7;
+  const edges = [
+    [6, 4],
+    [6, 2],
+    [5, 3],
+    [5, 4],
+    [3, 0],
+    [3, 1],
+    [3, 2],
+    [4, 1],
+  ];
+  const result = topologicalSortByBFS(vertices, edges);
+  const expected = [
+    [5, 6, 3, 4, 0, 1, 2],
+    [6, 5, 3, 4, 0, 1, 2],
+    [5, 6, 4, 3, 0, 2, 1],
+    [6, 5, 4, 3, 0, 1, 2],
+    [5, 6, 3, 4, 0, 2, 1],
+    [5, 6, 3, 4, 1, 2, 0],
+    [6, 5, 4, 3, 2, 1, 0],
+  ];
+  expect(expected).toContainEqual(result);
+});
+
+test('test9', () => {
+  const vertices = 7;
+  const edges = [
+    [6, 4],
+    [6, 2],
+    [5, 3],
+    [5, 4],
+    [3, 0],
+    [3, 1],
+    [3, 2],
+    [4, 1],
+    [1, 4],
+  ];
+  const result = topologicalSortByBFS(vertices, edges);
+  const expected = [];
+  expect(expected).toEqual(result);
 });
