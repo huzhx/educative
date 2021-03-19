3046e61f2601ef9283ec41152ca78742c6d1981f
Zhaoxian Hu <huzhx@uci.edu>
2021-03-19 15:55:21 -0700

    add strings-interleaving and its test

---
+++ strings-interleaving.js
@@ -0,0 +1,33 @@
+const isInterleaved = (m, n, p) => {
+  const memo = {};
+  return helper(m, n, p, 0, 0, 0);
+
+  function helper(m, n, p, mIndex, nIndex, pIndex) {
+    if (mIndex === m.length && nIndex === n.length && pIndex === p.length) return true;
+
+    if (pIndex === p.length) return false;
+
+    const subProblemKey = `${mIndex}-${nIndex}-${pIndex}`;
+
+    if (typeof memo[subProblemKey] !== 'undefined') {
+      return memo[subProblemKey];
+    }
+
+    let option1 = false;
+    let option2 = false;
+
+    if (mIndex < m.length && m[mIndex] === p[pIndex]) {
+      option1 = helper(m, n, p, mIndex + 1, nIndex, pIndex + 1);
+    }
+
+    if (nIndex < n.length && n[nIndex] === p[pIndex]) {
+      option2 = helper(m, n, p, mIndex, nIndex + 1, pIndex + 1);
+    }
+
+    memo[subProblemKey] = option1 || option2;
+
+    return memo[subProblemKey];
+  }
+};
+
+export default isInterleaved;
---
+++ strings-interleaving.test.js
@@ -0,0 +1,49 @@
+import isInterleaved from './strings-interleaving';
+
+test('test1', () => {
+  const m = 'abd';
+  const n = 'cef';
+  const p = 'abcdef';
+  const result = isInterleaved(m, n, p);
+  expect(result).toBeTruthy();
+});
+
+test('test2', () => {
+  const m = 'abd';
+  const n = 'cef';
+  const p = 'adcbef';
+  const result = isInterleaved(m, n, p);
+  expect(result).toBeFalsy();
+});
+
+test('test3', () => {
+  const m = 'abc';
+  const n = 'def';
+  const p = 'abdccf';
+  const result = isInterleaved(m, n, p);
+  expect(result).toBeFalsy();
+});
+
+test('test4', () => {
+  const m = 'abcdef';
+  const n = 'mnop';
+  const p = 'mnaobcdepf';
+  const result = isInterleaved(m, n, p);
+  expect(result).toBeTruthy();
+});
+
+test('test5', () => {
+  const m = 'aba';
+  const n = 'aaabaa';
+  const p = 'aabaabaaa';
+  const result = isInterleaved(m, n, p);
+  expect(result).toBeTruthy();
+});
+
+test('test6', () => {
+  const m = 'aba';
+  const n = 'aaabaa';
+  const p = 'aabbaaaaba';
+  const result = isInterleaved(m, n, p);
+  expect(result).toBeFalsy();
+});
