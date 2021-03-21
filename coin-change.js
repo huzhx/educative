a2f61501a6ad52a66193b4aaec9e6c1139dff259
Zhaoxian Hu <huzhx@uci.edu>
2021-03-21 01:16:06 -0700

    add top-down and bottom-up solutions

--- coin-change.js
+++ coin-change.js
@@ -26,4 +26,41 @@ const getNumOfCombinations = (denominations, total) => {
   }
 };
 
-export default getNumOfCombinations;
+const topDown = (denominations, total) => {
+  const memo = [];
+  return helper(denominations, 0, total);
+
+  function helper(denominations, curIndex, total) {
+    if (curIndex >= denominations.length) return 0;
+    if (total === 0) return 1;
+
+    memo[curIndex] = memo[curIndex] || [];
+    if (typeof memo[curIndex][total] !== 'undefined') {
+      return memo[curIndex][total];
+    }
+
+    let count1 = 0;
+    if (denominations[curIndex] <= total) {
+      count1 = helper(denominations, curIndex, total - denominations[curIndex]);
+    }
+
+    const count2 = helper(denominations, curIndex + 1, total);
+
+    memo[curIndex][total] = count1 + count2;
+    return memo[curIndex][total];
+  }
+};
+
+const bottomUp = (denominations, total) => {
+  const dp = new Array(total + 1).fill(0);
+  dp[0] = 1;
+  for (let i = 0; i < denominations.length; i++) {
+    const coin = denominations[i];
+    for (let j = coin; j <= total; j++) {
+      dp[j] += dp[j - coin];
+    }
+  }
+  return dp[dp.length - 1];
+};
+
+export { getNumOfCombinations, topDown, bottomUp };
--- coin-change.test.js
+++ coin-change.test.js
@@ -1,9 +1,49 @@
-import getNumOfCombinations from './coin-change';
+import { getNumOfCombinations, topDown, bottomUp } from './coin-change';
 
 test('test1', () => {
   const denominations = [1, 2, 3];
   const total = 5;
   const expected = 5;
   const result = getNumOfCombinations(denominations, total);
+  expect(result).toBe(expected);
+});
+
+test('test-topDown-1', () => {
+  const denominations = [1, 2, 3];
+  const total = 5;
+  const expected = 5;
+  const result = topDown(denominations, total);
+  expect(result).toBe(expected);
+});
+
+test('test-bottomUp-1', () => {
+  const denominations = [1, 2, 3];
+  const total = 5;
+  const expected = 5;
+  const result = bottomUp(denominations, total);
+  expect(result).toBe(expected);
+});
+
+test('test2', () => {
+  const denominations = [1, 2, 5];
+  const total = 7;
+  const expected = 6;
+  const result = getNumOfCombinations(denominations, total);
+  expect(result).toBe(expected);
+});
+
+test('test-topDown-2', () => {
+  const denominations = [1, 2, 5];
+  const total = 7;
+  const expected = 6;
+  const result = topDown(denominations, total);
+  expect(result).toBe(expected);
+});
+
+test('test-bottomUp-2', () => {
+  const denominations = [1, 2, 5];
+  const total = 7;
+  const expected = 6;
+  const result = bottomUp(denominations, total);
   expect(result).toBe(expected);
 });
