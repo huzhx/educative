8726a80d78cb5a0745c04da5cd72e4f5ff326b30
Zhaoxian Hu <huzhx@uci.edu>
2021-03-25 22:53:10 -0700

    add bottom-up solution

--- subset-sum.js
+++ subset-sum.js
@@ -18,4 +18,23 @@ const subsetSum = (nums, sum) => {
   }
 };
 
-export { subsetSum };
+const bottomUp = (nums, sum) => {
+  const dp = new Array(nums.length).fill(null).map((_) => new Array(sum + 1).fill(false));
+  for (let i = 0; i < dp.length; i++) {
+    dp[i][0] = true;
+  }
+  for (let s = 1; s <= sum; s++) {
+    dp[0][s] = nums[0] === s;
+  }
+  for (let i = 1; i < nums.length; i++) {
+    for (let s = 1; s <= sum; s++) {
+      dp[i][s] = dp[i - 1][s];
+      if (nums[i] <= s) {
+        dp[i][s] = dp[i][s] || dp[i - 1][s - nums[i]];
+      }
+    }
+  }
+  return dp[nums.length - 1][sum];
+};
+
+export { subsetSum, bottomUp };
--- subset-sum.test.js
+++ subset-sum.test.js
@@ -1,9 +1,16 @@
-import { subsetSum } from './subset-sum';
+import { subsetSum, bottomUp } from './subset-sum';
 
 test('test1', () => {
   const input = [1, 2, 3, 7];
   const sum = 6;
   const result = subsetSum(input, sum);
+  expect(result).toBeTruthy();
+});
+
+test('test-bottomUp-1', () => {
+  const input = [1, 2, 3, 7];
+  const sum = 6;
+  const result = bottomUp(input, sum);
   expect(result).toBeTruthy();
 });
 
@@ -14,9 +21,23 @@ test('test2', () => {
   expect(result).toBeTruthy();
 });
 
+test('test-bottomUp-2', () => {
+  const input = [1, 2, 7, 1, 5];
+  const sum = 10;
+  const result = bottomUp(input, sum);
+  expect(result).toBeTruthy();
+});
+
 test('test3', () => {
   const input = [1, 3, 4, 8];
   const sum = 6;
   const result = subsetSum(input, sum);
+  expect(result).toBeFalsy();
+});
+
+test('test-bottomUp-3', () => {
+  const input = [1, 3, 4, 8];
+  const sum = 6;
+  const result = bottomUp(input, sum);
   expect(result).toBeFalsy();
 });
