1474a487238718f5e5e1b8a51846e6ce609a5177
Zhaoxian Hu <huzhx@uci.edu>
2021-03-25 23:42:27 -0700

    add topDown solution

--- min-subset-sum-diff.js
+++ min-subset-sum-diff.js
@@ -19,4 +19,27 @@ const getMinDiff = (nums) => {
   }
 };
 
-export { getMinDiff };
+const topDown = (nums) => {
+  const total = nums.reduce((a, b) => a + b, 0);
+  const memo = [];
+  return helper(nums, 0, 0);
+
+  function helper(nums, curIndex, curSum) {
+    if (curIndex === nums.length) {
+      return Math.abs(curSum - (total - curSum));
+    }
+
+    memo[curIndex] = memo[curIndex] || [];
+    if (typeof memo[curIndex][curSum] !== 'undefined') {
+      return memo[curIndex][curSum];
+    }
+
+    const option1 = helper(nums, curIndex + 1, curSum + nums[curIndex]);
+    const option2 = helper(nums, curIndex + 1, curSum);
+
+    memo[curIndex][curSum] = Math.min(option1, option2);
+    return memo[curIndex][curSum];
+  }
+};
+
+export { getMinDiff, topDown };
--- min-subset-sum-diff.test.js
+++ min-subset-sum-diff.test.js
@@ -1,4 +1,4 @@
-import { getMinDiff } from './min-subset-sum-diff';
+import { getMinDiff, topDown } from './min-subset-sum-diff';
 
 test('test1', () => {
   const nums = [1, 2, 3, 9];
@@ -7,16 +7,37 @@ test('test1', () => {
   expect(result).toBe(expected);
 });
 
-test('test1', () => {
+test('test-topDown-1', () => {
+  const nums = [1, 2, 3, 9];
+  const result = topDown(nums);
+  const expected = 3;
+  expect(result).toBe(expected);
+});
+
+test('test2', () => {
   const nums = [1, 2, 7, 1, 5];
   const result = getMinDiff(nums);
   const expected = 0;
   expect(result).toBe(expected);
 });
 
-test('test1', () => {
+test('test-topDown-2', () => {
+  const nums = [1, 2, 7, 1, 5];
+  const result = topDown(nums);
+  const expected = 0;
+  expect(result).toBe(expected);
+});
+
+test('test3', () => {
   const nums = [1, 3, 100, 4];
   const result = getMinDiff(nums);
+  const expected = 92;
+  expect(result).toBe(expected);
+});
+
+test('test-topDown-3', () => {
+  const nums = [1, 3, 100, 4];
+  const result = topDown(nums);
   const expected = 92;
   expect(result).toBe(expected);
 });
