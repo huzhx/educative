88adb49117d2d9b538b65c9cfc05e820bfc841ea
Zhaoxian Hu <huzhx@uci.edu>
2021-03-27 15:10:32 -0700

    add topDown solution

--- staircase.js
+++ staircase.js
@@ -17,4 +17,21 @@ const getPossibleWaysNumber = (stairsNumber) => {
   }
 };
 
-export { getPossibleWaysNumber };
+const topDown = (stairsNumber) => {
+  const memo = [];
+  return helper(stairsNumber);
+
+  function helper(stairsNumber) {
+    if (stairsNumber < 0) return 0;
+    if (stairsNumber === 0) return 1;
+
+    if (typeof memo[stairsNumber] !== 'undefined') {
+      return memo[stairsNumber];
+    }
+
+    memo[stairsNumber] = helper(stairsNumber - 1) + helper(stairsNumber - 2) + helper(stairsNumber - 3);
+    return memo[stairsNumber];
+  }
+};
+
+export { getPossibleWaysNumber, topDown };
--- staircase.test.js
+++ staircase.test.js
@@ -1,4 +1,4 @@
-import { getPossibleWaysNumber } from './staircase';
+import { getPossibleWaysNumber, topDown } from './staircase';
 
 test('test1', () => {
   const stairsNumber = 3;
@@ -18,5 +18,26 @@ test('test3', () => {
   const stairsNumber = 5;
   const expected = 13;
   const result = getPossibleWaysNumber(stairsNumber);
+  expect(result).toBe(expected);
+});
+
+test('test-topDown-1', () => {
+  const stairsNumber = 3;
+  const expected = 4;
+  const result = topDown(stairsNumber);
+  expect(result).toBe(expected);
+});
+
+test('test-topDown-2', () => {
+  const stairsNumber = 4;
+  const expected = 7;
+  const result = topDown(stairsNumber);
+  expect(result).toBe(expected);
+});
+
+test('test-topDown-3', () => {
+  const stairsNumber = 5;
+  const expected = 13;
+  const result = topDown(stairsNumber);
   expect(result).toBe(expected);
 });
