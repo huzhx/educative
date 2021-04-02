91729d2a1505276e09af87fa6901c99eb8151f3c
Zhaoxian Hu <huzhx@uci.edu>
2021-04-02 00:18:01 -0700

    add topDown solution

--- house-thief.js
+++ house-thief.js
@@ -18,4 +18,27 @@ const getMaxMoney = (input) => {
   }
 };
 
-export { getMaxMoney };
+const topDown = (houses) => {
+  const memo = [];
+  return helper(houses, 0, -1);
+
+  function helper(houses, curIndex, prevIndex) {
+    if (curIndex >= houses.length) return 0;
+
+    memo[curIndex] = memo[curIndex] || [];
+    if (typeof memo[curIndex][prevIndex + 1] !== 'undefined') {
+      return memo[curIndex][prevIndex + 1];
+    }
+
+    let profit1 = -Infinity;
+    if (prevIndex === -1 || curIndex > prevIndex + 1) {
+      profit1 = houses[curIndex] + helper(houses, curIndex + 2, curIndex);
+    }
+    const profit2 = helper(houses, curIndex + 1, prevIndex);
+
+    memo[curIndex][prevIndex + 1] = Math.max(profit1, profit2);
+    return memo[curIndex][prevIndex + 1];
+  }
+};
+
+export { getMaxMoney, topDown };
--- house-thief.test.js
+++ house-thief.test.js
@@ -1,4 +1,4 @@
-import { getMaxMoney } from './house-thief';
+import { getMaxMoney, topDown } from './house-thief';
 
 test('test1', () => {
   const input = [2, 5, 1, 3, 6, 2, 4];
@@ -11,5 +11,19 @@ test('test2', () => {
   const input = [2, 10, 14, 8, 1];
   const expected = 18;
   const result = getMaxMoney(input);
+  expect(result).toBe(expected);
+});
+
+test('test-topDown-1', () => {
+  const input = [2, 5, 1, 3, 6, 2, 4];
+  const expected = 15;
+  const result = topDown(input);
+  expect(result).toBe(expected);
+});
+
+test('test-topDown-2', () => {
+  const input = [2, 10, 14, 8, 1];
+  const expected = 18;
+  const result = topDown(input);
   expect(result).toBe(expected);
 });
