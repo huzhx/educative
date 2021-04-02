b6f8af127888870c39616f696042d98e0efe5453
Zhaoxian Hu <huzhx@uci.edu>
2021-04-02 00:01:50 -0700

    add bottomUp solution

--- rod-cutting.js
+++ rod-cutting.js
@@ -52,4 +52,30 @@ const topDown = (lengths, prices, rodLength) => {
   }
 };
 
-export { getMaxProfit, topDown };
+const bottomUp = (lengths, prices, rodLength) => {
+  if (rodLength === 0 || prices.length === 0 || prices.length !== lengths.length) return 0;
+
+  const dp = new Array(lengths.length).fill(null).map((_) => new Array(rodLength + 1).fill(-Infinity));
+
+  for (let i = 0; i < lengths.length; i++) {
+    dp[i][0] = 0;
+  }
+
+  for (let i = 0; i < lengths.length; i++) {
+    for (let j = 1; j <= rodLength; j++) {
+      let profit1 = -Infinity;
+      let profit2 = -Infinity;
+      if (i > 0) {
+        profit1 = dp[i - 1][j];
+      }
+      if (lengths[i] <= j) {
+        profit2 = prices[i] + dp[i][j - lengths[i]];
+      }
+      dp[i][j] = Math.max(profit1, profit2);
+    }
+  }
+
+  return dp[lengths.length - 1][rodLength];
+};
+
+export { getMaxProfit, topDown, bottomUp };
--- rod-cutting.test.js
+++ rod-cutting.test.js
@@ -1,4 +1,4 @@
-import { getMaxProfit, topDown } from './rod-cutting';
+import { getMaxProfit, topDown, bottomUp } from './rod-cutting';
 
 test('test1', () => {
   const lengths = [1, 2, 3, 4, 5];
@@ -15,5 +15,14 @@ test('test-topDown-1', () => {
   const rodLength = 5;
   const expected = 14;
   const result = topDown(lengths, prices, rodLength);
+  expect(result).toBe(expected);
+});
+
+test('test-bottomUp-1', () => {
+  const lengths = [1, 2, 3, 4, 5];
+  const prices = [2, 6, 7, 10, 13];
+  const rodLength = 5;
+  const expected = 14;
+  const result = bottomUp(lengths, prices, rodLength);
   expect(result).toBe(expected);
 });
