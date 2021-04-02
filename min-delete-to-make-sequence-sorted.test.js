a89594f74efaba6aa209289c6a3b6cb6fc644aa2
Zhaoxian Hu <huzhx@uci.edu>
2021-04-02 14:07:36 -0700

    add bottomUp solution

--- min-delete-to-make-sequence-sorted.test.js
+++ min-delete-to-make-sequence-sorted.test.js
@@ -1,4 +1,4 @@
-import { getMinDeletes, topDown } from './min-delete-to-make-sequence-sorted';
+import { getMinDeletes, topDown, bottomUp } from './min-delete-to-make-sequence-sorted';
 
 test('test1', () => {
   const input = [4, 2, 3, 6, 10, 1, 12];
@@ -11,5 +11,12 @@ test('test-topDown-1', () => {
   const input = [4, 2, 3, 6, 10, 1, 12];
   const expected = 2;
   const result = topDown(input);
+  expect(result).toBe(expected);
+});
+
+test('test-bottomUp-1', () => {
+  const input = [4, 2, 3, 6, 10, 1, 12];
+  const expected = 2;
+  const result = bottomUp(input);
   expect(result).toBe(expected);
 });
