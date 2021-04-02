3a52563935a376090e72869e0ad8f2bf3e5f5c8c
Zhaoxian Hu <huzhx@uci.edu>
2021-04-02 13:49:30 -0700

    add topDown solution

--- min-delete-to-make-sequence-sorted.js
+++ min-delete-to-make-sequence-sorted.js
@@ -21,4 +21,27 @@ const getMinDeletes = (input) => {
   }
 };
 
-export { getMinDeletes };
+const topDown = (nums) => {
+  const memo = [];
+  return nums.length - helper(nums, 0, -1);
+
+  function helper(nums, curIndex, prevIndex) {
+    if (curIndex >= nums.length) return 0;
+
+    memo[curIndex] = memo[curIndex] || [];
+    if (typeof memo[curIndex][prevIndex + 1] !== 'undefined') {
+      return memo[curIndex][prevIndex + 1];
+    }
+
+    let count1 = 0;
+    if (prevIndex === -1 || nums[curIndex] >= nums[prevIndex]) {
+      count1 = 1 + helper(nums, curIndex + 1, curIndex);
+    }
+    const count2 = helper(nums, curIndex + 1, prevIndex);
+
+    memo[curIndex][prevIndex + 1] = Math.max(count1, count2);
+    return memo[curIndex][prevIndex + 1];
+  }
+};
+
+export { getMinDeletes, topDown };
--- min-delete-to-make-sequence-sorted.test.js
+++ min-delete-to-make-sequence-sorted.test.js
@@ -1,8 +1,15 @@
-import { getMinDeletes } from './min-delete-to-make-sequence-sorted';
+import { getMinDeletes, topDown } from './min-delete-to-make-sequence-sorted';
 
 test('test1', () => {
   const input = [4, 2, 3, 6, 10, 1, 12];
   const expected = 2;
   const result = getMinDeletes(input);
+  expect(result).toBe(expected);
+});
+
+test('test-topDown-1', () => {
+  const input = [4, 2, 3, 6, 10, 1, 12];
+  const expected = 2;
+  const result = topDown(input);
   expect(result).toBe(expected);
 });
